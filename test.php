b38c000e7b932cbdf0406614461c8b194e136253
yangzhiguo <yangzhiguo0903@163.com>
2012-11-30 17:35:48 +0800

    原来在phpstorm里用git跟用svn一样啊

---
+++ test.php
@@ -0,0 +1,35 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * matoin
+ *
+ * An open source application development framework for PHP 5.3 or newer
+ *
+ * @package    ${PACKAGE_NAME}
+ * @author     <yangzhiguo0903@gmail.com>
+ * @copyright  Copyright (c) 2010 - 2012, Yzg, Inc.
+ * @license    ${LICENSE_LINK}
+ * @link       ${PROJECT_LINK}
+ * @since      Version ${VERSION} 2012-11-30 下午5:34 $
+ * @filesource test.php
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * ${CLASS_NAME}
+ *
+ * ${CLASS_DESCRIPTION}
+ *
+ * @package     ${PACKAGE_NAME}
+ * @subpackage  ${SUBPACKAGE_NAME}
+ * @category    ${CATEGORY_NAME}
+ * @author      <yangzhiguo0903@gmail.com>
+ * @link        ${FILE_LINK}/test.php
+ */
+ 
+//start here......
+
+// END ${CLASS_NAME} class
+
+/* End of file test.php */
+/* Location: ${FILE_PATH}/test.php */
